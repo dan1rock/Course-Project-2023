8844de8e6b9cac49f88dd99117a0244ca5e4d401
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2023-12-29 01:06:49 +0200

    Implemented line charts

--- src/scenes/dashboard/context/dashboard.jsx
+++ src/scenes/dashboard/context/dashboard.jsx
@@ -1,9 +1,16 @@
+import LineChart from "../../../components/lineChart";
+import { mockLineData } from "../../../data/mockData";
+import { Box } from "@mui/material";
+
 const Dashboard = () => {
     return (
         <div id="main_content">
             <div className="main-title">
                 <p className="font-weight-bold">DASHBOARD</p>
             </div>
+            <Box height="250px" m="-20px 0 0 0" bgcolor="#ffffff" borderRadius="20px">
+                <LineChart data={mockLineData} bottomLegend="Data1" sideLegend="Data2"/>
+            </Box>
         </div>
     );
 }
--- src/scenes/dashboard/context/reporting.jsx
+++ src/scenes/dashboard/context/reporting.jsx
@@ -1,9 +1,24 @@
+import LineChart from "../../../components/lineChart";
+import {mockLineData} from "../../../data/mockData";
+import {Box} from "@mui/material";
+
 const Reporting = () => {
     return (
         <div id="main_content">
             <div className="main-title">
                 <p className="font-weight-bold">REPORTING</p>
             </div>
+            <Box display="flex">
+                <Box height="250px" m="-20px 10px 0 -5px" bgcolor="#ffffff" borderRadius="20px" flex="50%">
+                    <LineChart data={mockLineData} bottomLegend="Data1" sideLegend="Data2"/>
+                </Box>
+                <Box height="250px" m="-20px 0 0 0" bgcolor="#ffffff" borderRadius="20px" flex="50%">
+                    <LineChart data={mockLineData} bottomLegend="Data1" sideLegend="Data2"/>
+                </Box>
+            </Box>
+            <Box height="350px" m="10px 0 0 0" bgcolor="#ffffff" borderRadius="20px">
+                <LineChart data={mockLineData} bottomLegend="Data1" sideLegend="Data2"/>
+            </Box>
         </div>
     );
 }
