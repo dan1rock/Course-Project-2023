6c52d4ae0d0bf2fa692b0b163802b5bf34f8ccb0
Danylo Holets <90461399+dan1rock@users.noreply.github.com>
2023-12-28 18:37:26 +0200

    Finished users tab

--- src/scenes/dashboard/context/users.jsx
+++ src/scenes/dashboard/context/users.jsx
@@ -1,5 +1,5 @@
 import { DataGrid } from "@mui/x-data-grid";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { mockUsers } from "../../../data/mockData";
 
 const Users = () => {
@@ -35,6 +35,42 @@ const Users = () => {
             field: "access",
             headerName: "Access Level",
             flex: 1,
+            renderCell: ({row: {access}}) => {
+                let icon;
+                if (access === "admin") {
+                    icon = "shield";
+                } else if (access === "manager") {
+                    icon = "manage_accounts";
+                } else {
+                    icon = "person";
+                }
+                return (
+                    <Box
+                        width="150px"
+                        height="30px"
+                        m="0 auto"
+                        p="5 px"
+                        display="flex"
+                        justifyContent="center"
+                        color="#ffffff"
+                        backgroundColor={
+                            () => {
+                                if (access === "admin") return "#21232d"
+                                if (access === "manager") return "#4a4e64"
+                                return "#6a708f"
+                            }
+                        }
+                        borderRadius="4px"
+                    >
+                        <Box marginY="auto" ml="15px">
+                            <span className="material-icons-outlined">{icon}</span>
+                        </Box>
+                        <Typography  m="auto" ml="5px">
+                            {access}
+                        </Typography>
+                    </Box>
+                );
+            }
         }
         ];
 
